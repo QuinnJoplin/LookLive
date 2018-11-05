dcb364facd126db9575f9bc39394eeb0ed5679e2
Quinn Joplin <quinnjoplin@gmail.com>
2018-11-05 18:05:17 -0500

    Update HangMan.py

--- HangMan.py
+++ HangMan.py
@@ -1,199 +1,165 @@
-#include <fstream>
-#include <cstddef>
-#include <iostream>
-#include <string>
+import random
+name = ""
 
-using namespace std;
-struct WordType
-{
-public:
-  string word;
-  int count;
-};
+#Print's the intro messages
+def PrintMenu():
+  play = input("Would you like to play a game of Hangman? (Y/N)")
+  while (1):
+    if play == 'n' or play == 'N':
+      return None
+    if play == 'y' or play == 'Y':
+      name = input("Tell, me... what is your name? (First Last)")
+      return name
+    play = input("I'm sorry, I didn't catch that. Please repeat your answer. Y or n?")
 
-struct TreeNode
-{
-  WordType info;
-  TreeNode* left;
-  TreeNode* right;
-};
+#Pick's a word from the list
+def GenerateWord(words):
+  word = random.choice(words)
+  return word;
 
-class ListType
-{
-public:
-  ListType();
-  void InsertOrIncrement(string letters);
-  void getMaxRepeatedWord(ofstream&);
-  void PrintList(ofstream&);
-private:
-  TreeNode* root;
-};
+#Prints the Gallows and Hangman (if applicaple)
+def PrintStage(body):
+  print("|---\n|  |\n|  {0}\n| {1}{2}{3}\n|_{4}{5}\n|___|\n".format(body[0], body[1], body[2], body[3], body[4], body[5]))
 
-void getMax(TreeNode*& tree, int* count, string* word);
+#Checks User's guess for correctness
+def GuessCheck(correctWord, guess):
+  tmp = 0
+  indeces = []
+  for i in correctWord:
+    if guess == i:
+      indeces.append(tmp)
+      print("Correct!")
+    tmp += 1
+  if len(indeces) == 0:
+    print("Incorrect Guess!")
+    indeces.append(-1)
+    return indeces
+  return indeces
 
-string GetString(ifstream&);    
-const int MAX_LETTERS = 20;
-int main()
-{
-  ListType list;
-  string inFileName;
-  string outFileName;
-  string outputLabel;
-  ifstream inFile;
-  ofstream outFile;
-  string letters;
-  int minimumLength;
+#Adds incorrect guess to list
+def AddToUsed(used, letter):
+  used.append(letter)
+
+#Draws the "HangMan"
+def ChangeBody(strike, body):
+  def one():
+    body[0] = "O"
+  def two():
+    body[1] = "/"
+  def three():
+    body[2] = "|"
+  def four():
+    body[3] = "\\"
+  def five():
+    body[4] = "/"
+  def six():
+    body[5] = "\\"
   
-  // Prompt for file names, read file names, and prepare files
-  cout << "Enter name of input command file; press return." << endl;
-  cin  >> inFileName;
-  inFile.open(inFileName.c_str());
-  if (!inFile)
-    cout << "file not found";
+  options = {1: one, 2: two, 3:three, 4:four, 5:five, 6: six}
+  options[strike]()
 
-  cout << "Enter name of output file; press return." << endl;
-  cin  >> outFileName;
-  outFile.open(outFileName.c_str());
+def main():
+  guessed = False
+  words = {
+  "coyote": "Dingo of the wild west", 
+  "cowboy":"Rides a horse and wrangles cattle","dessert":"Hot, dry, and sandy",
+  "road runner":"meep, meep", 
+  "jackrabbit":'"What\'s up, Doc"', 
+  "horse":"The car that drives itself",
+  "duel":"Usually done in the middle of town at high noon", 
+  "revolver":"Classic six-shooter", 
+  "holster":"Stephen Colbert once called Trump one of these", 
+  "lasso":"Use this to wrap up them heifers", "bounty":"Think Duane `Dog' Chapman and Boba Fett","tumble weed":"Rolls through the dessert", "cactus":"covered in prickly spines", "sombrero":"Spanish for hat", "poncho":"Rhymes with honcho", 
+  "spurs":"Makes your horse go faster", 
+  "boots":"I just got me a pair of new..."}
+  name = PrintMenu().split()[0]
 
-  cout << "Enter name of test run; press return." << endl;
-  cin  >> outputLabel;
-  outFile << outputLabel << endl;
+  while (True):
+    strike = 0
+    correctWord = random.choice(list(words.keys()))
+    body = [" ", " ", " ", " ", "_", "_" ]
+    currentWord = []
+    used = []
 
-  cout << "Enter the minimum size word to be considered." << endl;
-  cin >> minimumLength;
+    print("Well then, %s, let's begin..." % name)
+
+    #Builds underscores for unknown letters
+    for i in correctWord:
+      if i != " ":
+        currentWord.append(" _ ")
+      else:
+        currentWord.append("   ")
+
+    
+    #Game Loop
+    while strike != 6 and guessed == False:
+      PrintStage(body)
+      print("Hint: ", words[correctWord])
+      i = 0
+      for x in currentWord:
+        print(currentWord[i], end = "")
+        i += 1
+      
+      #If the user has made at least one incorrect guess, the letters they've guessed incorrectly
+      if strike != 0:
+        print("Wrong Letters: ", end = "")
+        i = 0
+        for x in used:
+          print(used[i], end = ", ")
+          i += 1
+        print("\n")
+
+      #Takes In User Guess
+      guess = input("Try to guess one of the letters")
+      if len(guess) == 1 and guess >= 'a' and guess <= 'z': 
+
+        #User's Guess checked
+        index  = GuessCheck(correctWord, guess)
+        
+        tmp = False
+        for i in used:
+          if i == guess:
+            tmp = True
+        #Incorrect Guess
+        if index[0] == -1 and tmp != True:
+          strike += 1
+          AddToUsed(used, guess)
+          ChangeBody(strike, body)
+        else:
+          for x in index:
+            currentWord[x] = " {0} ".format(guess)
+
+
+        #Game Lost Check
+        if strike == 6:
+          PrintStage(body)
+          print("Game Over, the word was {0}!".format(correctWord))
+          tmp = input("Would you like to play again? (Y/N)")
+          if tmp == 'Y' or tmp == 'y':
+            break
+          if tmp != 'Y' and tmp != 'y':
+            return None
+
+        #Game Won Check
+        guessed = True
+        for x in currentWord:
+          if x == " _ ":
+            guessed = False
+        if guessed == True:
+          print("Congrats, you win! The word was {0}".format(correctWord))
+          tmp = input("Would you like to play again? (Y/N)")
+          if tmp != 'Y' and tmp != 'y':
+            return None
+          if tmp == 'Y' or tmp == 'y':
+            break
+        print("\n\n\n\n")
+      else:
+        print("Please repeat your guess [a-z]")
   
-  // Read the text stripping of words and inserting them into
-  // list.  If a word is already there, its count is incremented.
-  letters = GetString(inFile);
-  while (inFile)
-  {
-	if (letters.length() >= minimumLength)
-	  list.InsertOrIncrement(letters);  
-	letters = GetString(inFile);
-  }
-
-  list.PrintList(outFile);
-  list.getMaxRepeatedWord(outFile);
-  outFile.close();
-  inFile.close();
-  return 0;
-}
-
-string GetString(ifstream& inFile)
-// Post: Non alphanumeric characters as skipped. Alphanumeric characters are 
-//       read and concatenated on a string until a non-alphanumeric character is
-//       read or end of file is reached. Letters are all converted to lowercase.
-{
-  char letter;
-  string letters = "";  
-  
-  inFile.get(letter);
-  while (inFile && !isalnum(letter))
-    inFile.get(letter);
-  
-  if (!inFile)	
-    // No legal character found; empty string returned.
-     return letters;
-  else
-  {// Read and collect characters.
-    do
-    {
-	  letter = tolower(letter);
-      letters = letters + letter;
-      inFile.get(letter);
-    } while (isalnum(letter) && inFile);
-
-  }
-  return letters;
-}
 
 
-ListType::ListType()
-{
-  root = NULL;
-}
+    
   
 
-void Process(TreeNode*& tree, std::string letters)
-{
-  if (tree == NULL)
-  {
-    tree = new TreeNode;
-    tree->info.word = letters;
-    tree->info.count = 1;
-    tree->left = NULL;
-    tree->right = NULL;
-  }
-  else if (tree->info.word == letters) 
-    tree->info.count++;
-  else if (tree->info.word > letters)
-    Process(tree->left, letters);
-  else 
-    Process(tree->right, letters);
-}
 
-void ListType::InsertOrIncrement(string letters)
-{
-  Process(root, letters);
-}  
- 
-  
-void Print(TreeNode*& tree, ofstream& outFile)
-{
-  
-  if (tree != NULL)
-  {
-    Print(tree->left, outFile);
-    outFile << tree->info.word;
-    outFile  << " " << tree->info.count;
-	outFile << endl;
-    Print(tree->right, outFile);
-  }
-}
-
-void ListType::PrintList(ofstream& outFile)
-{
-  Print(root, outFile);
-}
-
-void ListType::getMaxRepeatedWord(ofstream& outFile)
-{
-  int count = 0;
-  string word = "test";
-  getMax(root, &count, &word);
-  outFile << "Max Word: " << word << " " << count;
-}
-
-void getMax(TreeNode*& tree, int* count, string* word)
-{
-  if (tree != NULL)
-  {
-    if (tree->info.count >= *count) {
-      *count = tree->info.count;
-      *word = tree->info.word;
-
-      getMax(tree->left, count, word);
-      getMax(tree->right, count, word);
-
-    }
-  }
-}
-
-// string getMax(TreeNode*& tree, int* count, string* word)
-// {
-
-//   // if (tree == NULL) {
-//   //   return *word;
-//   // }
-
-//   // if (tree->info.count > count) {
-//   //   *count = tree->info.count;
-//   //   *word = tree->info.word;
-//   // }
-
-//   // string left = getMax(tree->left, count, string);
-//   // string right = getMax(tree->right, count, string);
-//   // if (left > right){
-//   //   return left
-//   // }
-// }
+main()
